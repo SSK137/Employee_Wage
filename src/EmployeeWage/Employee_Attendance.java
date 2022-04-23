bf42d5fe344c4639aef2b6d545e599fb2f1942cb
SSK137 <saurabhkodam@gmail.com>
2022-04-23 12:29:46 +0530

    Employee Attendance marks

--- src/EmployeeWage/Employee_Attendance.java
+++ src/EmployeeWage/Employee_Attendance.java
@@ -1,4 +1,17 @@
 package EmployeeWage;
 
+import java.util.Random;
+
 public class Employee_Attendance {
+    public static void main(String[] args) {
+        int absent;
+        Random rdm = new Random();
+        int ab = rdm.nextInt();
+        absent=ab%2;
+        if(absent == 0){
+            System.out.println("Employee is Absent");
+        }else {
+            System.out.println("Employee is Present");
+        }
+    }
 }
